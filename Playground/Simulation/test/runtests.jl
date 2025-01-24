746b2ca198c25143d2cd9dac2784d68e2bf86f21
Fabian Weik <fabian.weik@protonmail.com>
2025-01-24 13:02:35 +0100

    use officially endorsed structure

--- Playground/Simulation/test/runtests.jl
+++ Playground/Simulation/test/runtests.jl
@@ -3,13 +3,5 @@ using Test
 using Simulation
 
 @testset verbose = true "parsing" begin
-    @testset "parse proprietary configuration" begin
-        base_path = "../Data/Proprietary Format"
-        @test try (topology, scenario) = Simulation.Configuration.ProprietaryFormat.load(base_path)
-            true
-        catch e
-            @show e
-            false
-        end
-    end
+    include("parsing_tests.jl")
 end
