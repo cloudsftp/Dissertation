a31fd7ce50a8dc328c04a5e3115b95cb259caa58
Fabian Weik <fabian.weik@protonmail.com>
2025-01-25 12:16:00 +0100

    split test cases for converting formats

--- Playground/Simulation/test/FormatConversion/ToCustomTests.jl
+++ Playground/Simulation/test/FormatConversion/ToCustomTests.jl
@@ -23,27 +23,40 @@ function assert_equals_unordered(actual::Vector, expected::Vector)
     )
 end
 
-@testset "unordered vector compare" begin
+@testset "compare unordered vectors" begin
     assert_equals_unordered([1, 2, 3], [1, 2, 3])
     assert_equals_unordered([1, 2, 3], [1, 3, 2])
     assert_equals_unordered([1, 2, 2, 3], [1, 2, 3, 2])
 end
 
-function assert_equals_unordered(actual::CF.Topology, expected::CF.Topology)
-    assert_equals_unordered(expected.nodes, actual.nodes)
-    assert_equals_unordered(expected.pipes, actual.pipes)
-    assert_equals_unordered(expected.consumers, actual.consumers)
-    assert_equals_unordered(expected.sources, actual.sources)
+function test_to_custom(proprietary, custom)
+    actual = to_custom(proprietary)
+    if actual isa Vector && custom isa Vector
+        assert_equals_unordered(actual, custom)
+    else
+        @test actual == custom
+    end
 end
 
-@testset "convert topology to custom format"  begin
-    proprietary_topology = PF.Topology(
+@testset "convert nodes to custom format" begin
+    test_to_custom(
         Dict(
             "VL001" => PF.Node([1.0, 1.0, 3.0], true),
             "VL002" => PF.Node([1.0, 2.0, 3.0], true),
             "VL003" => PF.Node([2.0, 1.0, 3.0], true),
             "VL004" => PF.Node([2.0, 2.0, 3.0], true),
         ),
+        [
+            CF.Node("VL001", CF.Position(1.0, 1.0, 3.0), true),
+            CF.Node("VL002", CF.Position(1.0, 2.0, 3.0), true),
+            CF.Node("VL003", CF.Position(2.0, 1.0, 3.0), true),
+            CF.Node("VL004", CF.Position(2.0, 2.0, 3.0), true),
+        ],
+    )
+end
+
+@testset "convert pipes to custom format" begin
+    test_to_custom(
         Dict(
             "PF001" => PF.Pipe(
                 ["VL001", "VL002"],
@@ -62,52 +75,62 @@ end
                 1.2, 1, 1, 1, 1,
             ),
         ),
-        Dict(
-            "C1" => PF.Consumer(["VL004", "VL001"]),
-        ),
-        Dict(
-            "S1" => PF.Source(["VL004", "VL001"]),
-        )
-    )
-    actual = to_custom(proprietary_topology)
-
-    expected = CF.Topology(
-        [
-            CF.Node("VL001", CF.Position(1.0, 1.0, 3.0), true),
-            CF.Node("VL002", CF.Position(1.0, 2.0, 3.0), true),
-            CF.Node("VL003", CF.Position(2.0, 1.0, 3.0), true),
-            CF.Node("VL004", CF.Position(2.0, 2.0, 3.0), true),
-        ],
         [
             CF.Pipe("PF001", 1.2, 1, 1, 1, 1, "VL001", "VL002"),
             CF.Pipe("PF002", 1.2, 1, 1, 1, 1, "VL001", "VL003"),
             CF.Pipe("PF003", 1.2, 1, 1, 1, 1, "VL002", "VL004"),
             CF.Pipe("PF004", 1.2, 1, 1, 1, 1, "VL003", "VL004"),
         ],
-        [CF.Consumer("C1", "VL004", "VL001")],
-        [CF.Source("S1", "VL004", "VL001")],
     )
-
-    assert_equals_unordered(actual, expected)
 end
 
-function assert_equals_unordered(actual::CF.Scenario, expected::CF.Scenario)
-    @test expected.settings == actual.settings
-
-    assert_equals_unordered(expected.signals, actual.signals)
-    assert_equals_unordered(expected.inputs, actual.inputs)
-    assert_equals_unordered(expected.consumers, actual.consumers)
-    assert_equals_unordered(expected.sources, actual.sources)
-    assert_equals_unordered(expected.pipes, actual.pipes)
+@testset "convert consumes to custom form" begin
+    test_to_custom(
+        Dict(
+            "C1" => PF.Consumer(["VL004", "VL001"]),
+            "C2" => PF.Consumer(["VL014", "VL101"]),
+            "C3" => PF.Consumer(["VL004", "VL051"]),
+        ),
+        [
+            CF.Consumer("C1", "VL004", "VL001"),
+            CF.Consumer("C2", "VL014", "VL101"),
+            CF.Consumer("C3", "VL004", "VL051"),
+        ],
+    )
 end
 
-@testset "convert topology to custom format"  begin
-    proprietary_scenario = PF.Scenario(
+@testset "convert sources to custom format"  begin
+    test_to_custom(
+        Dict(
+            "S1" => PF.Source(["VL004", "VL001"]),
+            "S2" => PF.Source(["VL034", "VL201"]),
+            "S3" => PF.Source(["VL004", "VL031"]),
+        ),
+        [
+            CF.Source("S1", "VL004", "VL001"),
+            CF.Source("S2", "VL034", "VL201"),
+            CF.Source("S3", "VL004", "VL031"),
+        ],
+    )
+end
+
+@testset "convert settings to custom format"  begin
+    test_to_custom(
         PF.Settings(
             100, 60, 10,
             0, 4.8, 15, 8,
             1000, 1e-6,
         ),
+        CF.Settings(
+            100, 60, 10,
+            0, 4.8, 15, 8,
+            1000, 1e-6,
+        ),
+    )
+end
+
+@testset "convert signals to custom format"  begin
+    test_to_custom(
         Dict(
             "one" => PF.Signal(
                 "CONSTANT",
@@ -125,30 +148,6 @@ end
                 ]
             ),
         ),
-        Dict(
-            "input1" => PF.Input(["one", "signal1"]),
-        ),
-        Dict(
-            "C1" => PF.ConsumerSignal(60, 2, "input1"),
-        ),
-        Dict(
-            "S1" => PF.SourceSignal("Source2", "input1"),
-        ),
-        Dict(
-            "PF001" => PF.PipeSignal("input1"),
-            "PF002" => PF.PipeSignal("input1"),
-            "PF003" => PF.PipeSignal("input1"),
-            "PF004" => PF.PipeSignal("input1"),
-        ),
-    )
-    actual = to_custom(proprietary_scenario)
-
-    expected = CF.Scenario(
-        CF.Settings(
-            100, 60, 10,
-            0, 4.8, 15, 8,
-            1000, 1e-6,
-        ),
         [
             CF.SignalConst(
                 "one", 1, 1.,
@@ -162,29 +161,10 @@ end
                 ]
             ),
         ],
-        [
-            CF.Input("input1", ["one", "signal1"]),
-        ],
-        [
-            CF.ConsumerInputMapping(
-                "C1", "input1", 60, 2,
-            ),
-        ],
-        [
-            CF.InputMapping("S1", "input1"),
-        ],
-        [
-            CF.InputMapping("PF001", "input1"),
-            CF.InputMapping("PF002", "input1"),
-            CF.InputMapping("PF003", "input1"),
-            CF.InputMapping("PF004", "input1"),
-        ]
     )
-
-    assert_equals_unordered(actual, expected)
 end
 
-@testset "quick check" begin
+@testset "convert signals to custom format throws exception when type is unknown" begin
     signals = Dict(
         "signal1" => PF.Signal(
             "CUBIC",
@@ -199,4 +179,56 @@ end
     )
 
     @test_throws "signal type \"CUBIC\" unknown" to_custom(signals)
+end
+
+@testset "convert inputs to custom format"  begin
+    test_to_custom(
+        Dict(
+            "input1" => PF.Input(["one", "signal1"]),
+        ),
+        [
+            CF.Input("input1", ["one", "signal1"]),
+        ],
+    )
+end
+
+@testset "convert consumer signals to custom format"  begin
+    test_to_custom(
+        Dict(
+            "C1" => PF.ConsumerSignal(60, 2, "input1"),
+        ),
+        [
+            CF.ConsumerInputMapping(
+                "C1", "input1", 60, 2,
+            ),
+        ],
+    )
+end
+
+@testset "convert producer signals to custom format"  begin
+    test_to_custom(
+        Dict(
+            "S1" => PF.SourceSignal("Source2", "input1"),
+        ),
+        [
+            CF.InputMapping("S1", "input1"),
+        ],
+    )
+end
+
+@testset "convert pipe signals to custom format"  begin
+    test_to_custom(
+        Dict(
+            "PF001" => PF.PipeSignal("input1"),
+            "PF002" => PF.PipeSignal("input1"),
+            "PF003" => PF.PipeSignal("input1"),
+            "PF004" => PF.PipeSignal("input1"),
+        ),
+        [
+            CF.InputMapping("PF001", "input1"),
+            CF.InputMapping("PF002", "input1"),
+            CF.InputMapping("PF003", "input1"),
+            CF.InputMapping("PF004", "input1"),
+        ],
+    )
 end
