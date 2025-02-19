bb0fc44edc9791b7370662664f9485b838f56527
Fabian Weik <fabian.weik@protonmail.com>
2025-02-19 15:12:39 +0100

    WIP compute A_r^p

---
+++ Playground/rimulation/src/simulation/mod.rs
@@ -0,0 +1,58 @@
+mod hydraulic;
+
+use anyhow::Error;
+use hydraulic::Matrices;
+use nalgebra::DVector;
+
+use crate::{
+    types::{
+        formats::custom::Settings,
+        network::{Network, Node},
+    },
+    water,
+};
+
+fn initial_energy_densities(network: &Network, settings: &Settings) -> Result<Vec<f64>, Error> {
+    network
+        .nodes()
+        .map(|node| -> Result<f64, anyhow::Error> {
+            water::energy_density(match node {
+                Node::Pressure { temperature, .. } => temperature.value_at(0.)?,
+                _ => settings.feed_temperature,
+            })
+        })
+        .collect()
+}
+
+pub fn simulate(network: Network, settings: Settings) -> Result<(), Error> {
+    dbg!(&network);
+
+    let e = DVector::from_vec(initial_energy_densities(&network, &settings)?);
+
+    let matrices = Matrices::try_from(&network)?;
+
+    let q = DVector::from_vec(
+        network
+            .demand_nodes
+            .iter()
+            .map(|node| match node {
+                Node::Pressure { .. } => {
+                    unreachable!("there should be no pressure node included here")
+                }
+                Node::Demand { demand, .. } => demand.value_at(0.), // TODO: transform to velocity
+                Node::Zero { .. } => Ok(0.),
+            })
+            .collect::<Result<Vec<f64>, Error>>()?,
+    );
+
+    let m1 = (matrices.ar.transpose() * matrices.at.transpose())
+        .lu()
+        .solve(&q)
+        .expect("could not solve system of equations for m1");
+
+    dbg!(m1);
+
+    let v = DVector::from_fn(network.num_nodes(), |i, _| todo!());
+
+    todo!();
+}
