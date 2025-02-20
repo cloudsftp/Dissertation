a49046e1651bc47e13fc5ab33178de6ade11cd0a
Fabian Weik <fabian.weik@protonmail.com>
2025-02-20 21:31:07 +0100

    finally finish transition function

--- Playground/rimulation/src/main.rs
+++ Playground/rimulation/src/main.rs
@@ -1,47 +1,16 @@
-use std::{fs, io::Write};
-
 use anyhow::Error;
 use rimulation::{
     simulation::simulate,
-    transition::{transition, transition_fast},
+    transition::transition_cubic,
     types::{formats::custom::load, network::Network},
 };
 
-fn sim() -> Result<(), Error> {
+fn main() -> Result<(), Error> {
     let network = load("data/running_example")?;
     let settings = network.scenario.settings.clone();
     let network: Network = network.try_into()?;
 
     simulate(network, settings)?;
-
-    Ok(())
-}
-
-fn trans() -> Result<(), Error> {
-    let mut file = fs::File::create("/tmp/transition_data")?;
-
-    let n = 1000usize;
-    let dx = 2.;
-
-    let yl = 0.;
-    let dyl = 0.;
-    let yr = 1.;
-    let dyr = 0.5;
-
-    for i in 0..n {
-        let x = -dx + i as f64 * 2. * dx / n as f64;
-        let y = transition(x, -dx, dx, yl, dyl, yr, dyr);
-        let y_fast = transition_fast(x, -dx, dx, yl, dyl, yr, dyr);
-
-        file.write(format!("{} {} {}\n", x, y, y_fast).as_bytes())?;
-    }
-
-    Ok(())
-}
-
-fn main() -> Result<(), Error> {
-    //sim()?;
-    trans()?;
 
     Ok(())
 }
--- Playground/rimulation/src/transition.rs
+++ Playground/rimulation/src/transition.rs
@@ -2,62 +2,99 @@ use nalgebra::{matrix, vector};
 
 use crate::polynome::poly;
 
-pub fn transition(x: f64, l: f64, r: f64, yl: f64, dyl: f64, yr: f64, dyr: f64) -> f64 {
-    let a = matrix![
-        1.,  l,  l * l,  l * l * l;
-        0., 1., 2. * l, 3. * l * l;
-        1.,  r,  r * r,  r * r * r;
-        0., 1., 2. * r, 3. * r * r;
-    ];
+pub fn transition_cubic(
+    x: f64,
+    left: f64,
+    right: f64,
+    p_0: f64,
+    m_0: f64,
+    p_1: f64,
+    m_1: f64,
+) -> f64 {
+    let width = right - left;
 
-    let d = vector![yl, dyl, yr, dyr];
+    let t = ((x - left) / width).clamp(0., 1.);
 
-    let p = a.lu().solve(&d).expect("should be able to solve");
-    poly(x, p.data.as_slice())
-}
+    let m_0 = m_0 * width;
+    let m_1 = m_1 * width;
 
-// WIP: faster
-pub fn transition_fast(x: f64, l: f64, r: f64, yl: f64, dyl: f64, yr: f64, dyr: f64) -> f64 {
-    let x = if x < l {
-        0.
-    } else if x > r {
-        1.
-    } else {
-        (x - l) / (r - l)
-    };
+    let cubic_term = 2. * p_0 + m_0 - 2. * p_1 + m_1;
+    let quadratic_term = -3. * p_0 - 2. * m_0 + 3. * p_1 - m_1;
 
-    let a = yl;
-    let b = dyl;
-    let d = 2. * yl + dyl - 2. * yr + dyr;
-    let c = -3. * yl - 2. * dyl + 3. * yr - dyr;
-
-    poly(x, &[a, b, c, d])
+    poly(t, &[p_0, m_0, quadratic_term, cubic_term])
 }
 
 #[cfg(test)]
 mod tests {
+    use anyhow::Error;
     use approx::assert_relative_eq;
+    use std::fs;
+    use std::io::Write;
 
     use super::*;
 
     #[test]
-    fn transition_values() {
-        let n = 10usize;
-        let dx = 2.;
+    fn generate_transition_data() {
+        let left = -5.;
+        let right = 5.;
 
-        let yl = 0.;
-        let dyl = 0.5;
-        let yr = 1.;
-        let dyr = 2.;
+        let (p_0, m_0) = (0., 0.2);
+        let (p_1, m_1) = (1., 0.3);
+
+        let mut file = fs::File::create("/tmp/transition_data").expect("could not open file");
+
+        let n = 1000usize;
 
         for i in 0..n {
-            let x = -dx + i as f64 * 2. * dx / n as f64;
-            let y = transition(x, -dx, dx, yl, dyl, yr, dyr);
-            let y_fast = transition_fast(x, -dx, dx, yl, dyl, yr, dyr);
+            let x = left + i as f64 * (right - left) / n as f64;
+            let y = transition_cubic(x, left, right, p_0, m_0, p_1, m_1);
 
-            dbg!(i);
-
-            assert_relative_eq!(y, y_fast);
+            file.write(format!("{} {}\n", x, y).as_bytes())
+                .expect("could not write to file");
         }
+    }
+
+    #[test]
+    fn cubic_boundary_values() {
+        let left = -5.;
+        let right = 5.;
+
+        let (p_0, m_0) = (-1., 0.2);
+        let (p_1, m_1) = (5., 0.3);
+
+        assert_eq!(transition_cubic(-5., left, right, p_0, m_0, p_1, m_1), -1.);
+        assert_eq!(transition_cubic(5., left, right, p_0, m_0, p_1, m_1), 5.);
+    }
+
+    #[test]
+    fn cubic_boundary_slopes() {
+        let left = -5.;
+        let right = 5.;
+
+        let (p_0, m_0) = (-1., 0.2);
+        let (p_1, m_1) = (5., 0.3);
+
+        let assert_slope = |x, expected: f64| {
+            let mut dx = 1e-6;
+            if x == right {
+                dx = -dx;
+            }
+
+            let fx = transition_cubic(x, left, right, p_0, m_0, p_1, m_1);
+            let fdx = transition_cubic(x + dx, left, right, p_0, m_0, p_1, m_1);
+
+            let slope = ((fdx - fx) / dx);
+            let difference = (slope - expected).abs();
+            assert!(
+                difference < dx.abs(),
+                "expected difference of slopes to be smaller than {}: was {} and expected {}",
+                dx,
+                slope,
+                expected,
+            );
+        };
+
+        assert_slope(-5., 0.2);
+        assert_slope(5., 0.3);
     }
 }
