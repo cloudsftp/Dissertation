cad8c90072fb5daa2422ff24e69e6dbdb17d9349
Fabian Weik <fabian.weik@protonmail.com>
2025-03-17 11:14:10 +0100

    remove comments in output module

--- rimulation/src/output/mod.rs
+++ rimulation/src/output/mod.rs
@@ -9,9 +9,6 @@ use crate::types::{
     network::Network,
 };
 
-const CHARACTERS_PER_VALUE: usize = 12;
-const CHARACTERS_PER_TIME: usize = 10;
-
 pub fn write_temperatures<EdgeParameters>(
     network: &Network<EdgeParameters>,
     settings: &Settings,
@@ -45,53 +42,6 @@ pub fn write_temperatures<EdgeParameters>(
     }
 
     writer.flush()?;
-
-    /*
-    let header = network
-        .nodes()
-        .enumerate()
-        .filter_map(|(i, node)| demand_node_indices.contains(&i).then(|| node.get_name()))
-        .fold(String::new(), |mut acc, name| {
-            if acc.is_empty() {
-                for _ in 0..(CHARACTERS_PER_TIME - 1) {
-                    acc.push(' ');
-                }
-                acc.push('t');
-            }
-            for _ in 0..(CHARACTERS_PER_VALUE - name.len() + 1) {
-                acc.push(' ');
-            }
-            acc.push_str(&name);
-            acc
-        });
-
-    writer.write(header.as_bytes())?;
-    writer.write("\n\n".as_bytes())?;
-
-    for t in 0..settings.num_steps() {
-        let values = result.iter().map(|(_, temperatures)| temperatures[t]).fold(
-            String::new(),
-            |mut acc, value| {
-                let value = format!("{:.5}", value);
-                if acc.is_empty() {
-                    let time = (t as f64 * settings.time_step).to_string();
-                    for _ in 0..(CHARACTERS_PER_TIME - time.len()) {
-                        acc.push(' ');
-                    }
-                    acc.push_str(time.as_str());
-                }
-                for _ in 0..(CHARACTERS_PER_VALUE - value.len() + 1) {
-                    acc.push(' ');
-                }
-                acc.push_str(&value);
-                acc
-            },
-        );
-
-        writer.write(values.as_bytes())?;
-        writer.write("\n".as_bytes())?;
-    }
-    */
 
     Ok(())
 }
