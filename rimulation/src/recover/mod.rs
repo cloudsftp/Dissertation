a32ef67289c187a12d74312293ae029ecc1a1983
Fabian Weik <fabian.weik@protonmail.com>
2025-03-21 11:26:51 +0100

    compute discrete offset of signals

--- rimulation/src/recover/mod.rs
+++ rimulation/src/recover/mod.rs
@@ -1,5 +1,7 @@
+use std::{f64::INFINITY, usize::MAX};
+
 use anyhow::{anyhow, Error};
-use nalgebra::DVector;
+use nalgebra::{DVector, Normed};
 
 use crate::types::{
     formats::custom::Settings,
@@ -47,56 +49,135 @@ fn compute_delays(
     signal: DVector<f64>,
     source_signals: &Vec<(Node, DVector<f64>)>,
 ) -> Result<Vec<(usize, usize)>, Error> {
-    let signal_length = signal.len();
+    let n = signal.len();
 
-    todo!()
+    if source_signals.len() != 1 {
+        return Err(anyhow!("multiple sources not supported yet"));
+    }
+
+    let (source_id, source_signal) = source_signals[0].clone();
+
+    let mut min_dist = INFINITY;
+    let mut opt_offset = MAX;
+    for offset in 0..n - 2 {
+        let dist = delay_distance(&signal, &source_signal, offset);
+
+        dbg!(offset, dist);
+
+        if dist < min_dist {
+            min_dist = dist;
+            opt_offset = offset;
+        }
+    }
+
+    Ok(vec![(0, opt_offset)])
 }
 
 fn delay_distance(
-    signal_a: DVector<f64>,
-    signal_b: DVector<f64>,
+    signal_a: &DVector<f64>,
+    signal_b: &DVector<f64>,
     offset: usize, // f64?
 ) -> f64 {
-    // Compute distance of signals when a offset is applied
-    // this should scale with the overlap of the signals
-    // distance([1, 1]) == distance([1])
-    todo!()
+    let n = signal_a.len();
+    let signal_a = signal_a.rows(0, n - offset);
+    let signal_b = signal_b.rows(offset, n - offset);
+
+    (signal_a - signal_b).norm()
 }
 
 #[cfg(test)]
 mod tests {
+    use approx::{assert_relative_eq, relative_eq};
+
     use crate::types::{formats::custom::Position, network::test::DUMMY_CONST_SIGNAL};
 
     use super::*;
 
     #[test]
-    fn compute_delays_of_two_signals() {
+    fn compute_distance_of_signals() {
         struct TestCase<'a> {
             name: &'a str,
             signal_a: &'a [f64],
             signal_b: &'a [f64],
-            delay: usize,
+            offset: usize,
+            distance: f64,
         }
 
-        let test_cases = [TestCase {
-            name: "simple",
-            signal_a: &[0., 0., 0.],
-            signal_b: &[0., 0., 0.],
-            delay: 0,
-        }];
+        let test_cases = [
+            TestCase {
+                name: "no offset",
+                signal_a: &[60., 60., 120., 120., 120., 120.],
+                signal_b: &[60., 60., 60., 120., 120., 120.],
+                offset: 0,
+                distance: 60.,
+            },
+            TestCase {
+                name: "perfect offset",
+                signal_a: &[60., 60., 120., 120., 120., 120.],
+                signal_b: &[60., 60., 60., 120., 120., 120.],
+                offset: 1,
+                distance: 1.,
+            },
+        ];
 
         for TestCase {
             name,
             signal_a,
             signal_b,
-            delay,
+            offset,
+            distance,
+        } in test_cases
+        {
+            let signal_a = DVector::from_column_slice(signal_a);
+            let signal_b = DVector::from_column_slice(signal_b);
+
+            let measured = delay_distance(&signal_a, &signal_b, offset);
+
+            if !relative_eq!(measured, distance) {
+                panic!(
+                    "distance not as expected {} != {} in test case '{}'",
+                    measured, distance, name
+                )
+            }
+        }
+    }
+
+    #[test]
+    fn compute_delays_of_signals() {
+        struct TestCase<'a> {
+            name: &'a str,
+            signal_a: &'a [f64],
+            signal_b: &'a [f64],
+            expected_delay: usize,
+        }
+
+        let test_cases = [
+            TestCase {
+                name: "simple",
+                signal_a: &[60., 60., 120., 120., 120., 120.],
+                signal_b: &[60., 60., 60., 120., 120., 120.],
+                expected_delay: 1,
+            },
+            TestCase {
+                name: "simple",
+                signal_a: &[60., 60., 120., 120., 120., 120.],
+                signal_b: &[60., 60., 60., 90., 120., 120.],
+                expected_delay: 1,
+            },
+        ];
+
+        for TestCase {
+            name,
+            signal_a,
+            signal_b,
+            expected_delay,
         } in test_cases
         {
             let signal_a = DVector::from_row_slice(signal_a);
             let signal_b = DVector::from_row_slice(signal_b);
 
             let delays = compute_delays(
-                signal_b,
+                signal_a,
                 &vec![(
                     Node::Pressure {
                         name: String::from("Source"),
@@ -108,14 +189,12 @@ mod tests {
                             z: 0.,
                         },
                     },
-                    signal_a,
+                    signal_b,
                 )],
             )
             .expect("error when computing delays");
 
-            dbg!(delays);
-
-            panic!()
+            assert_eq!(vec![(0, expected_delay)], delays)
         }
     }
 }
